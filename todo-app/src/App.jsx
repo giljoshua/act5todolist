2f2c5e16442c2505d598aebd39e45a95a7da33f6
giljoshua <anchetajoshuagil@gmail.com>
2025-03-07 15:56:31 +0800

    Add files via upload

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,15 @@
+import TodoList from "./components/ToDoList"; 
+ 
+function App() { 
+  return ( 
+    <div className="app-wrapper"> 
+      <h1 className="app-title">
+        <span className="title-icon">✓</span>
+        <span className="title-text">React TODO App</span>
+      </h1> 
+      <TodoList /> 
+    </div> 
+  ); 
+} 
+ 
+export default App;
